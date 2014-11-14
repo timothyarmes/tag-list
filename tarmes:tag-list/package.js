7fa08a9ee5bc5cbb8fa5d650722f1a2c950fac59
Timothy Armes <tim@timothyarmes.com>
2014-11-14 12:09:55 +0100

    Added supplied ID to containing DIV

--- tarmes:tag-list/package.js
+++ tarmes:tag-list/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'tarmes:tag-list',
   summary: "Display a list to which the user can add or remove items, supporting hot code push and Typeahead",
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/timothyarmes/tag-list'
 });
 
